73047cda6bc5a8f247981cfdcbac1d2547488ab0
ereyes0212 <erick.reyes@tiempo.hn>
2025-06-02 13:58:43 -0600

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+
+// next.config.js
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com'],
+    },
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            config.plugins.push(new PrismaPlugin());
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
