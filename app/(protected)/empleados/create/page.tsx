58c69bb8d0a0f6ed3caa14468fb403c82e57981b
ereyes0212 <erick.reyes@tiempo.hn>
2025-06-05 13:54:49 -0600

    carga masiva

--- app/(protected)/empleados/create/page.tsx
+++ app/(protected)/empleados/create/page.tsx
@@ -1,11 +1,11 @@
-import HeaderComponent from "@/components/HeaderComponent";
-import { PlusCircle } from "lucide-react";
 import { getSession, getSessionPermisos } from "@/auth";
-import { redirect } from "next/navigation";
-import { EmpleadoFormulario } from "../components/Form"; // Asegúrate de que el formulario sea para Empleados
+import HeaderComponent from "@/components/HeaderComponent";
 import NoAcceso from "@/components/noAccess";
+import { PlusCircle } from "lucide-react";
+import { redirect } from "next/navigation";
+import { getPuestosActivas } from "../../puestos/actions";
 import { getEmpleados } from "../actions";
-import {getPuestosActivas } from "../../puestos/actions";
+import { EmpleadoFormulario } from "../components/Form"; // Asegúrate de que el formulario sea para Empleados
 
 export default async function Create() {
   const permisos = await getSessionPermisos();
@@ -24,20 +24,32 @@ export default async function Create() {
 
   // Determina qué lista de puestos cargar según el rol del usuario
   const puestos = await getPuestosActivas()
-  const empleados =  await getEmpleados()
+  const empleados = await getEmpleados()
 
 
   const initialData = {
+    id: "",
     nombre: "",
     apellido: "",
     correo: "",
-    activo: true,
+    genero: "Masculino", // Valor por defecto
+    activo: true, // Por defecto, el nuevo empleado está activo
+    fechaNacimiento: new Date(),
     nombreUsuario: "",
-    genero: "", // Adding the 'genero' property
-    fechaNacimiento: new Date(), // Adding the 'fechaNacimiento' property with the current date
-    jefe_id:"",
-    puesto_id:"",
+    jefe_id: "", // Por defecto, no tiene jefe asignado
+    puesto_id: "", // Por defecto, no tiene puesto asignado
+    numeroIdentificacion: "",
+    fechaIngreso: new Date(), // Fecha de ingreso por defecto es hoy
+    departamentoDomicilio: "",
+    ciudadDomicilio: "",
+    direccionDomicilio: "",
+    telefono: "",
+    jefe: undefined, // Por defecto, no tiene jefe asignado
+    colonia: "",
+    profesion: "",
+    vacaciones: 0 // Por defecto, 0 días de vacaciones
   };
+
 
   return (
     <div>
@@ -47,7 +59,7 @@ export default async function Create() {
         screenName="Crear Empleado" // Cambié la pantalla a "Crear Empleado"
       />
       <EmpleadoFormulario
-        puestos={puestos || []} 
+        puestos={puestos || []}
         jefe={empleados}
         isUpdate={false} // Esto es para indicar que estamos creando, no actualizando
         initialData={initialData} // Datos iniciales para crear un nuevo empleado
