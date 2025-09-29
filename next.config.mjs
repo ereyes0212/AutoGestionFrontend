2e29f02b3f878185381143a9e89417b2d8cda3aa
ereyes0212 <erick.reyes@tiempo.hn>
2025-09-29 14:55:51 -0600

    cambios en el docker

--- next.config.mjs
+++ next.config.mjs
@@ -19,6 +19,21 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    experimental: {
+        preloadEntriesOnStart: false,
+    },
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+        if (config.cache && !dev) {
+            config.cache = Object.freeze({
+                type: 'memory',
+            })
+        }
+        // Important: return the modified config
+        return config
+    },
 };
 
 export default nextConfig;
